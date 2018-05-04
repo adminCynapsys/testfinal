61ca21f6687767c0eb3a5a50e1417161e7397f6f
admin <a.chaari@cynapsys.de>
2018-05-04 10:37:55 +0200

    first

---
+++ src/main/java/de/cynapsys/BackDemo2Application.java
@@ -0,0 +1,12 @@
+package de.cynapsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackDemo2Application.class, args);
+	}
+}
---
+++ src/test/java/de/cynapsys/BackDemo2ApplicationTests.java
@@ -0,0 +1,16 @@
+package de.cynapsys;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackDemo2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
